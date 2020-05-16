8f0278e6c9bb60861f7b6c5e436b09a0f230d9bd
MalindaK <kumarasinghe@axinom.com>
2020-05-16 11:20:44 +0530

    Code refactored and added Grid and Container components.

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Grid } from "../Grid/Grid";
+
+export const Container: React.FC = () => {
+  return <Grid />;
+};
---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./Grid.module.scss";
+
+export const Grid: React.FC = () => {
+  return <div className={classes.container}></div>;
+};
